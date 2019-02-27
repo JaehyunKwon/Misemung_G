f0ae19050351677cfeace61f2dcb3939318c7f15
JaehyunKwon <jhk911108@naver.com>
2019-02-27 14:52:45 +0900

    [권재현] 리스트 갱신시 stationName 사라지는 부분 수정, 카울리 종료 팝업 광고 추가

--- app/src/main/java/kr/com/misemung/network/GetTransCoordTask.java
+++ app/src/main/java/kr/com/misemung/network/GetTransCoordTask.java
@@ -7,7 +7,9 @@ import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import kr.com.misemung.realm.repository.CityRepository;
 import kr.com.misemung.ui.MainActivity;
+import kr.com.misemung.vo.CityInfo;
 import kr.com.misemung.vo.GeoInfo;
 
 /**
@@ -116,6 +118,13 @@ public class GetTransCoordTask {	// 스레드
         handler.post(() -> {
 
             active=false;
+
+			CityInfo info = new CityInfo();
+			info.setTmX(getX);
+			info.setTmY(getY);
+
+			// DB에 저장
+			CityRepository.City.set(addr, info);
 
             MainActivity.TransCoordThreadResponse(getX, getY, addr);
 
--- app/src/main/java/kr/com/misemung/ui/MainActivity.java
+++ app/src/main/java/kr/com/misemung/ui/MainActivity.java
@@ -4,6 +4,7 @@ package kr.com.misemung.ui;
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
@@ -22,14 +23,20 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.fsn.cauly.CaulyAdInfo;
+import com.fsn.cauly.CaulyAdInfoBuilder;
+import com.fsn.cauly.CaulyCloseAd;
+import com.fsn.cauly.CaulyCloseAdListener;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
@@ -75,7 +82,8 @@ import kr.com.misemung.vo.CityInfo;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 		, GoogleApiClient.OnConnectionFailedListener
 		, GoogleApiClient.ConnectionCallbacks
-		, SwipeRefreshLayout.OnRefreshListener {
+		, SwipeRefreshLayout.OnRefreshListener
+		, CaulyCloseAdListener {
 
 
 	private SwipeRefreshLayout mSwipeRefreshLayout;	// 아래로 드래그 후 새로고침하는 레이아웃
@@ -99,6 +107,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 	private EditText where;
 	private ListView searchListView;
+	private TextView searchNoData;
 	private Button this_place;
 	public static String stationName = "";
 
@@ -114,12 +123,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 	private FragmentContainerHelper mFramentContainerHelper;
 
+	// 마지막으로 뒤로가기 버튼을 눌렀던 시간 저장
+	private long backKeyPressedTime = 0;
+	// 첫 번째 뒤로가기 버튼을 누를때 표시
+	private Toast toast;
+
+	private static final String APP_CODE = "CAULY";	// 테스트용
+	//private static final String APP_CODE = "iR75C70S";	// 상용
+
+	private CaulyCloseAd mCloseAd;
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_main);
+
+		// CloseAd 초기화
+		CaulyAdInfo closeAdInfo = new CaulyAdInfoBuilder(APP_CODE).build();
+		mCloseAd = new CaulyCloseAd();
+
+		/*
+		 * Optional //원하는 버튼의 문구를 설젇 할 수 있다. mCloseAd.setButtonText("취소", "종료");
+		 * //원하는 텍스트의 문구를 설젇 할 수 있다. mCloseAd.setDescriptionText("종료하시겠습니까?");
+		 */
+		mCloseAd.setAdInfo(closeAdInfo);
+		mCloseAd.setCloseAdListener(this);
 
         initLayout();
 	}
@@ -136,7 +166,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         magicIndicator = findViewById(R.id.magic_indicator);
 		viewPager = findViewById(R.id.viewpager);
 		where = findViewById(R.id.where);
-        searchListView = findViewById(R.id.searchListView);
+        searchListView = findViewById(R.id.search_list_view);
+        searchNoData = findViewById(R.id.search_no_data);
 		this_place = findViewById(R.id.this_place);
 
 		where.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
@@ -335,10 +366,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 	 * 검색된 도시 API 조회 결과
 	 * */
 	public void SearchCityThreadResponse(ArrayList<CityInfo> cityInfos) {    //측정소 정보를 가져온 결과
+		if (searchNoData.getVisibility() == View.VISIBLE) {
+			searchNoData.setVisibility(View.GONE);
+		}
+
 		searchListView.setVisibility(View.VISIBLE);
 
-		searchAdapter = new SearchAdapter(this, cityInfos);
-		searchListView.setAdapter(searchAdapter);
+		if (cityInfos.size() > 0) {
+			searchAdapter = new SearchAdapter(this, cityInfos);
+			searchListView.setAdapter(searchAdapter);
+		} else {
+			searchNoData.setVisibility(View.VISIBLE);
+		}
+
+		where.setText("");
 
 		GetSearchCityListThread.active = false;
 		GetSearchCityListThread.interrupted();
@@ -520,8 +561,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 		CityRecord record = CityRepository.City.selectByCityData(viewPager.getCurrentItem()+1);
 		if (record != null) {
+			stationName = record.umdName;
 			getNearStation(record.tmX, record.tmY);
 		}
+	}
+
+	@Override
+	protected void onResume() {
+		if (mCloseAd != null)
+			mCloseAd.resume(this);
+		super.onResume();
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			showCloseAd(null);
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	public void showCloseAd(View button)
+	{
+		// 앱을 처음 설치하여 실행할 때, 필요한 리소스를 다운받았는지 여부.
+		if (mCloseAd != null && mCloseAd.isModuleLoaded()) {
+			mCloseAd.show(this);
+			//광고의 수신여부를 체크한 후 노출시키고 싶은 경우, show(this) 대신 request(this)를 호출.
+			//onReceiveCloseAd에서 광고를 정상적으로 수신한 경우 , show(this)를 통해 광고 노출
+
+		} else {
+			// 광고에 필요한 리소스를 한번만 다운받는데 실패했을 때 앱의 종료팝업 구현
+			showDefaultClosePopup();
+		}
+	}
+
+	private void showDefaultClosePopup() {
+		new AlertDialog.Builder(this).setTitle("").setMessage("종료 하시겠습니까?")
+				.setPositiveButton("예", (dialog, which) -> finish()).setNegativeButton("아니오", null).show();
+	}
+
+	// CaulyCloseAdListener
+	@Override
+	public void onFailedToReceiveCloseAd(CaulyCloseAd ad, int errCode,
+										 String errMsg) {
+
+	}
+
+	// CloseAd의 광고를 클릭하여 앱을 벗어났을 경우 호출되는 함수이다.
+	@Override
+	public void onLeaveCloseAd(CaulyCloseAd ad) {
+	}
+
+	// CloseAd의 request()를 호출했을 때, 광고의 여부를 알려주는 함수이다.
+	@Override
+	public void onReceiveCloseAd(CaulyCloseAd ad, boolean isChargable) {
+
+	}
+
+	// 왼쪽 버튼을 클릭 하였을 때, 원하는 작업을 수행하면 된다.
+	@Override
+	public void onLeftClicked(CaulyCloseAd ad) {
+
+	}
+
+	// 오른쪽 버튼을 클릭 하였을 때, 원하는 작업을 수행하면 된다.
+	// Default로는 오른쪽 버튼이 종료로 설정되어있다.
+	@Override
+	public void onRightClicked(CaulyCloseAd ad) {
+		finish();
+	}
+
+	@Override
+	public void onShowedCloseAd(CaulyCloseAd ad, boolean isChargable) {
+
 	}
 }
 
